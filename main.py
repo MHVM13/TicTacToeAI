d315b3f626d32300b0223cf9b6662e604b2bc4df
Maksim Mikheev <space1engine@gmail.com>
2022-11-27 23:34:34 +0300

    Add field. Add print_field() function check_draw(), check_win() and insert_value()

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+# If we try 4,3,7,8 positions ...the cods shows it as draw. But actually the bot wins there. SECOND VID
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+field = {
+    1: ' ', 2: ' ', 3: ' ',
+    4: ' ', 5: ' ', 6: ' ',
+    7: ' ', 8: ' ', 9: ' ',
+}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def print_field():
+    print(f"{field[1]} {field[2]} {field[3]}")
+    print(f"{field[4]} {field[5]} {field[6]}")
+    print(f"{field[7]} {field[8]} {field[9]}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def is_free(pos):
+    return True if field[pos] == ' ' else False
+
+
+def check_draw():
+    for key in field.keys():
+        if key == ' ':  # Если есть еще свободные клетки
+            return False
+
+    return True
+
+
+def check_win():
+    if field[1] == field[2] == field[3] and field[1] == field[3] and not is_free(1):
+        return True
+    elif field[4] == field[5] and field[4] == field[6] and not is_free(4):
+        return True
+    elif field[7] == field[8] and field[7] == field[9] and not is_free(7):
+        return True
+    elif field[1] == field[4] and field[1] == field[7] and not is_free(1):
+        return True
+    elif field[2] == field[5] and field[2] == field[8] and not is_free(2):
+        return True
+    elif field[3] == field[6] and field[3] == field[9] and not is_free(3):
+        return True
+    elif field[1] == field[5] and field[1] == field[9] and not is_free(1):
+        return True
+    elif field[7] == field[5] and field[7] == field[3] and not is_free(7):
+        return True
+    else:
+        return False
+
+
+def insertValue(pos, value):
+    if is_free(pos):
+        field[pos] = value
+        print_field()
+
+        if check_draw():
+            print('Draw!')
+            exit()
+
+        if check_win():
+            if value == 'x':
+                print('Bot win!')
+                exit()
+            else:
+                print('Player win!')
+                exit()
+    else:
+        print('Enter another position')
+        insertValue(int(input('New position: ')))
+        return
+
